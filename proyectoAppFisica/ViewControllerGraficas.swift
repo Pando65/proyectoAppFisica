75dd33228de4e289e97c0041eb4a5efc0d762a0f
Omar Manjarrez <omjrrz@outlook.com>
2016-11-01 10:36:04 -0600

    refactor modificaTiempo function

--- proyectoAppFisica/ViewControllerGraficas.swift
+++ proyectoAppFisica/ViewControllerGraficas.swift
@@ -30,6 +30,7 @@ class ViewControllerGraficas: UIViewController {
     var longitudPlataforma: CGFloat = 0.0
     var imagen : UIImage!
     var arrPosiciones : [Double] = []
+    var arrVelocidades : [Double] = []
     var graficaPosicion : ViewControllerGraficaPosicion!
     
     override func viewDidLoad() {
@@ -71,9 +72,8 @@ class ViewControllerGraficas: UIViewController {
         let tiempoAntiguo = tiempo
         tiempo = Int(sender.value)
         
-        let velocidad = velocidadInicial + aceleracion * Double(tiempo)
-        let posActual = posInicial + velocidadInicial * Double(tiempo) + 0.5 * aceleracion * (Double(tiempo) * Double(tiempo))
-        
+        let posActual = arrPosiciones[tiempo]
+        let velocidad = arrVelocidades[tiempo]
         if posActual >= -20.0 && posActual <= 20.0 {
             let posRelativa = ((CGFloat(posActual) + 20.0) / 40.0) * longitudPlataforma
             
@@ -113,15 +113,16 @@ class ViewControllerGraficas: UIViewController {
 
     // MAR: - Functions
     func llenarArreglos() {
-        //var velocidad = velocidadInicial
+        var velocidad = velocidadInicial
         var posicion = posInicial
         var tiempo = 0
         repeat {
             posicion = posInicial + velocidadInicial * Double(tiempo) + 0.5 * aceleracion * (Double(tiempo) * Double(tiempo))
+            velocidad = velocidadInicial + aceleracion * Double(tiempo)
             arrPosiciones.append(posicion)
+            arrVelocidades.append(velocidad)
             tiempo += 1
         } while posicion >= -20 && posicion <= 20
-        print(arrPosiciones)
         graficaPosicion.yValues = arrPosiciones
     }
     func setImagesPositionAndSize() {
